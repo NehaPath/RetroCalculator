fd80045fe7d74250e31543722fc0b004ddf4107d
Neha Pathmanaban <neha.pathmanaban@gmail.com>
2017-03-11 22:19:30 -0500

    calculator logic

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -11,7 +11,23 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var outputLabel: UILabel!
+    
     var btnSound: AVAudioPlayer!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +43,36 @@ class ViewController: UIViewController {
             print(err.debugDescription)
         }
         
+        outputLabel.text = "0"
+        
     }
+    
     
     @IBAction func numberPressed(sender: UIButton){
         playSound()
+        
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject){
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject){
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject){
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject){
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
     }
     
     func playSound() {
@@ -40,6 +82,42 @@ class ViewController: UIViewController {
         
         btnSound.play()
     }
+    
+    func processOperation(operation: Operation){
+        playSound()
+        if currentOperation != Operation.Empty {
+            
+            //user selected an operator and then selected another operator
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                    
+                } else if currentOperation == Operation.Divide {
+                      result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                    
+                } else if currentOperation == Operation.Subtract {
+                      result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                    
+                } else if currentOperation == Operation.Add {
+                      result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLabel.text = result
+        }
+            
+            currentOperation = operation
+        } else {
+            //This is the first time an operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        
+        }
 
 }
 
+}
